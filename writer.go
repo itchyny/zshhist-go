d26bb723a1bda5b35213ba319b973985e4f18300
itchyny <itchyny@hatena.ne.jp>
2018-10-07 00:57:45 +0900

    refactor Writer

--- writer.go
+++ writer.go
@@ -38,35 +38,24 @@ func (w *Writer) Write(h History) (err error) {
 	if err != nil {
 		return
 	}
-	idx := strings.IndexRune(h.Command, '\n')
-	if idx < 0 {
-		_, err = w.out.Write([]byte(Metafy(h.Command)))
+	start, idx := 0, strings.IndexRune(h.Command, '\n')
+	for {
+		if idx < 0 {
+			_, err = w.out.Write([]byte(Metafy(h.Command[start:])))
+			if err != nil {
+				return
+			}
+			break
+		}
+		_, err = w.out.Write([]byte(Metafy(h.Command[start : start+idx])))
 		if err != nil {
 			return
 		}
-	} else {
-		start := 0
-		for {
-			if idx < 0 {
-				_, err = w.out.Write([]byte(Metafy(h.Command[start:])))
-				if err != nil {
-					return
-				}
-				break
-			}
-			_, err = w.out.Write([]byte(Metafy(h.Command[start : start+idx])))
-			if err != nil {
-				return
-			}
-			_, err = w.out.Write([]byte{'\\', '\n'})
-			if err != nil {
-				return
-			}
-			start, idx = start+idx+1, strings.IndexRune(h.Command[start+idx+1:], '\n')
+		_, err = w.out.Write([]byte{'\\', '\n'})
+		if err != nil {
+			return
 		}
-	}
-	if err != nil {
-		return
+		start, idx = start+idx+1, strings.IndexRune(h.Command[start+idx+1:], '\n')
 	}
 	_, err = w.out.Write([]byte{'\n'})
 	return err
