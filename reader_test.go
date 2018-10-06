59bd1bc47692639196812d0931b813eaf24d03d7
itchyny <itchyny@hatena.ne.jp>
2018-10-07 00:45:45 +0900

    refactor test for Reader

--- reader_test.go
+++ reader_test.go
@@ -6,29 +6,33 @@ import (
 	"testing"
 )
 
-func TestReader(t *testing.T) {
-	r := strings.NewReader(`: 1537447916:0;echo hello, world!
+var (
+	histString = `: 1537447916:0;echo hello, world!
 : 1537448167:1;echo hello\
 echo world\
 
 : 1537448170:0;go version
 : 1537448316:0;echo ` + "\xe3\x81\x83\xb3\xe3\x82\x83\xb3\xe3\x81\xab\xe3\x81\x83\x81\xe3\x81\xaf\xe3\x80\x81\xe4\xb8\x83\xb6\xe7\x83\xb5\x83\xac" + `
-`)
-	decoder := NewReader(r)
-	var xs []History
-	for decoder.Scan() {
-		xs = append(xs, decoder.History())
-	}
-	if decoder.Err() != nil {
-		t.Fatal(decoder.Err())
-	}
-	expected := []History{
+`
+	histories = []History{
 		{1537447916, 0, "echo hello, world!"},
 		{1537448167, 1, "echo hello\necho world\n"},
 		{1537448170, 0, "go version"},
 		{1537448316, 0, "echo こんにちは、世界"},
 	}
-	if !reflect.DeepEqual(xs, expected) {
-		t.Errorf("expected: %+v, got: %+v\n", expected, xs)
+)
+
+func TestReader(t *testing.T) {
+	r := strings.NewReader(histString)
+	reader := NewReader(r)
+	var xs []History
+	for reader.Scan() {
+		xs = append(xs, reader.History())
+	}
+	if reader.Err() != nil {
+		t.Fatal(reader.Err())
+	}
+	if !reflect.DeepEqual(xs, histories) {
+		t.Errorf("expected: %+v, got: %+v\n", histories, xs)
 	}
 }
