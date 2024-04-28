e828d6a5f67e6b3251ae073329f07184eb448f6f
Michael Lorant <michael.lorant@nine.com.au>
2024-04-28 18:11:18 +1000

    Improve reader and writer testing
    Both the reader and writer were restrictive in adding extra tests due
    to their existing implementation. By converting these tests into test
    tables, more tests can be added (included failure states).
    
    It was necessary to convert both the reader and writer tests at the
    same time as they were sharing global variables (which have been
    removed).
    
    The history variables contained metafied characters which made it
    difficult to handle as a Go string and required combining with other
    encoding styles. As part of this improvement, golden files were used
    for the history file as this allowed it to be shared between the reader
    and writer tests as well as solve the issues with encoding.

--- reader_test.go
+++ reader_test.go
@@ -1,47 +1,51 @@
 package zshhist
 
 import (
+	"os"
+	"path"
 	"reflect"
-	"strings"
 	"testing"
 )
 
-var (
-	histString = `: 1537447916:0;echo hello, world!
-: 1537448167:1;echo hello\
-echo world\
-
-: 1537448170:0;go version
-: 1537448316:0;echo ` + "\xe3\x81\x83\xb3\xe3\x82\x83\xb3\xe3\x81\xab\xe3\x81\x83\x81\xe3\x81\xaf\xe3\x80\x81\xe4\xb8\x83\xb6\xe7\x83\xb5\x83\xac" + `
-: 1537448503:0;echo a\
-b\
-c\
-
-: 1537448453:0;echo ` + "\x22\xe3\x81\x82\xe3\x81\x83\xa4\xe3\x81\x83\xa6\xe3\x81\x83\xa8\xe3\x81\x83\xaa" + `\
-` + "\xe3\x81\x83\xab\xe3\x81\x83\xad\xe3\x81\x83\xaf\xe3\x81\x83\xb1\xe3\x81\x83\xb3" + `\
-` + "\xe3\x81\x83\xb5\xe3\x81\x83\xb7\xe3\x81\x83\xb9\xe3\x81\x83\xbb\xe3\x81\x83\xbd\x22" + `
-`
-	histories = []History{
-		{1537447916, 0, "echo hello, world!"},
-		{1537448167, 1, "echo hello\necho world\n"},
-		{1537448170, 0, "go version"},
-		{1537448316, 0, "echo こんにちは、世界"},
-		{1537448503, 0, "echo a\nb\nc\n"},
-		{1537448453, 0, "echo \"あいうえお\nかきくけこ\nさしすせそ\""},
-	}
-)
-
 func TestReader(t *testing.T) {
-	r := strings.NewReader(histString)
-	reader := NewReader(r)
-	var xs []History
-	for reader.Scan() {
-		xs = append(xs, reader.History())
+	testCases := map[string]struct {
+		file      string
+		histories []History
+	}{
+		"extended": {
+			file: "extended.histfile",
+			histories: []History{
+				{1537447916, 0, "echo hello, world!"},
+				{1537448167, 1, "echo hello\necho world\n"},
+				{1537448170, 0, "go version"},
+				{1537448316, 0, "echo こんにちは、世界"},
+				{1537448503, 0, "echo a\nb\nc\n"},
+				{1537448453, 0, "echo \"あいうえお\nかきくけこ\nさしすせそ\""},
+			},
+		},
 	}
-	if reader.Err() != nil {
-		t.Fatal(reader.Err())
-	}
-	if !reflect.DeepEqual(xs, histories) {
-		t.Errorf("expected: %+v, got: %+v\n", histories, xs)
+
+	for name, tt := range testCases {
+		t.Run(name, func(t *testing.T) {
+			f, err := os.Open(path.Join("testdata", tt.file))
+			if err != nil {
+				t.Fatalf("unable to open testdata file: %v: %v", tt.file, err.Error())
+			}
+			defer f.Close()
+
+			var got []History
+
+			r := NewReader(f)
+			for r.Scan() {
+				got = append(got, r.History())
+			}
+			if r.Err() != nil {
+				t.Fatalf("unable to scan testdata: %v", r.Err())
+			}
+
+			if !reflect.DeepEqual(tt.histories, got) {
+				t.Errorf("\nwant:\n%+v\ngot:\n%+v\n", tt.histories, got)
+			}
+		})
 	}
 }
--- writer_test.go
+++ writer_test.go
@@ -2,19 +2,49 @@ package zshhist
 
 import (
 	"bytes"
+	"os"
+	"path"
 	"testing"
 )
 
 func TestWriter(t *testing.T) {
-	out := new(bytes.Buffer)
-	writer := NewWriter(out)
-	for _, h := range histories {
-		err := writer.Write(h)
-		if err != nil {
-			t.Fatal(err)
-		}
+	testCases := map[string]struct {
+		file      string
+		histories []History
+	}{
+		"extended": {
+			file: "extended.histfile",
+			histories: []History{
+				{1537447916, 0, "echo hello, world!"},
+				{1537448167, 1, "echo hello\necho world\n"},
+				{1537448170, 0, "go version"},
+				{1537448316, 0, "echo こんにちは、世界"},
+				{1537448503, 0, "echo a\nb\nc\n"},
+				{1537448453, 0, "echo \"あいうえお\nかきくけこ\nさしすせそ\""},
+			},
+		},
 	}
-	if histString != out.String() {
-		t.Errorf("expected: %+v, got: %+v\n%+v\n%+v", histString, out.String(), []byte(histString), []byte(out.String()))
+
+	for name, tt := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := new(bytes.Buffer)
+			w := NewWriter(got)
+
+			for _, h := range tt.histories {
+				err := w.Write(h)
+				if err != nil {
+					t.Fatalf("unable to write: %v", err.Error())
+				}
+			}
+
+			want, err := os.ReadFile(path.Join("testdata", tt.file))
+			if err != nil {
+				t.Fatalf("unable to read testdata file: %v: %v", tt.file, err.Error())
+			}
+
+			if string(want) != got.String() {
+				t.Errorf("\nwant:\n%v\ngot:\n%v\n", string(want), got.String())
+			}
+		})
 	}
 }
