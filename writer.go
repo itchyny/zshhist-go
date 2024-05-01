291c791e5ec7b17c467fac4dcdab75ba6a680ae6
itchyny <itchyny@cybozu.co.jp>
2024-05-01 19:28:27 +0900

    improve performance of writer using bufio.Writer

--- writer.go
+++ writer.go
@@ -1,6 +1,7 @@
 package zshhist
 
 import (
+	"bufio"
 	"io"
 	"strconv"
 	"strings"
@@ -8,66 +9,55 @@ import (
 
 // A Writer writes history to a writer.
 type Writer struct {
-	out io.Writer
+	w   *bufio.Writer
+	buf [24]byte
 }
 
 // NewWriter creates a new writer.
 func NewWriter(w io.Writer) *Writer {
-	return &Writer{out: w}
+	return &Writer{w: bufio.NewWriter(w)}
 }
 
 // Write a history to the writer.
 func (w *Writer) Write(h History) (err error) {
 	if h.Time != 0 {
-		if err = w.extended(h); err != nil {
-			return err
+		if err = w.writeExtended(h); err != nil {
+			return
 		}
 	}
 
-	start, idx := 0, strings.IndexRune(h.Command, '\n')
-	for {
-		if idx < 0 {
-			_, err = w.out.Write([]byte(Metafy(h.Command[start:])))
+	for cmd := h.Command; ; {
+		i := strings.IndexByte(cmd, '\n')
+		if i < 0 {
+			_, err = w.w.WriteString(Metafy(cmd))
 			if err != nil {
 				return
 			}
 			break
 		}
-		_, err = w.out.Write([]byte(Metafy(h.Command[start : start+idx])))
+		_, err = w.w.WriteString(Metafy(cmd[:i]))
 		if err != nil {
 			return
 		}
-		_, err = w.out.Write([]byte{'\\', '\n'})
+		_, err = w.w.WriteString("\\\n")
 		if err != nil {
 			return
 		}
-		start, idx = start+idx+1, strings.IndexRune(h.Command[start+idx+1:], '\n')
+		cmd = cmd[i+1:]
 	}
-	_, err = w.out.Write([]byte{'\n'})
-	return err
+
+	err = w.w.WriteByte('\n')
+	if err != nil {
+		return
+	}
+	err = w.w.Flush()
+	return
 }
 
-func (w *Writer) extended(h History) (err error) {
-	_, err = w.out.Write([]byte{':', ' '})
-	if err != nil {
-		return err
-	}
-	_, err = w.out.Write([]byte(strconv.Itoa(h.Time)))
-	if err != nil {
-		return err
-	}
-	_, err = w.out.Write([]byte{':'})
-	if err != nil {
-		return err
-	}
-	_, err = w.out.Write([]byte(strconv.Itoa(h.Elapsed)))
-	if err != nil {
-		return err
-	}
-	_, err = w.out.Write([]byte{';'})
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (w *Writer) writeExtended(h History) (err error) {
+	buf := append(w.buf[:0], ':', ' ')
+	buf = append(strconv.AppendInt(buf, int64(h.Time), 10), ':')
+	buf = append(strconv.AppendInt(buf, int64(h.Elapsed), 10), ';')
+	_, err = w.w.Write(buf)
+	return
 }
